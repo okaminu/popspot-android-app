ffba21350656f16836304b986a61666fadf49be5
Andrius <andriusdgt@gmail.com>
2015-07-13 17:47:18 +0300

    main project

--- app/src/main/java/com/jaguarsoft/mac/favouriteplaces/MapsActivity.java
+++ app/src/main/java/com/jaguarsoft/mac/favouriteplaces/MapsActivity.java
@@ -24,7 +24,7 @@ public class MapsActivity extends FragmentActivity {
         super.onResume();
         setUpMapIfNeeded();
     }
-//aa
+
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
